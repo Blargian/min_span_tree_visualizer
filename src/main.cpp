216985891911a0b8fd0a7fa4a537aece4665972f
Graham Struwig <shaunstruwig.ss@gmail.com>
2023-02-12 09:39:54 +0200

    in progress: build currently failing due to find_package glfw3 error in CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 ﻿#include <iostream>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl2.h"
+#include <stdio.h>
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#endif
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+static void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
 
 int main()
 {
-	std::cout << "Hello min span tree visualizer." << std::endl;
+    glfwSetErrorCallback(glfw_error_callback);
+
+    if (!glfwInit())
+        return 1;
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "MST Visualizer", NULL, NULL);
+    if (window == NULL)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    while (!glfwWindowShouldClose(window))
+    {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 	return 0;
 };
