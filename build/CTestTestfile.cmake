8c3fe957c6a183001eb643fb8bc88a9a43a2cc7d
Graham Struwig <shaunstruwig.ss@gmail.com>
2023-02-13 11:16:27 +0200

    :see_no_evil: updated gitignore to not stage changes for the build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gstru/Desktop/min_span_tree_visualizer
-# Build directory: C:/Users/gstru/Desktop/min_span_tree_visualizer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/gstru/Desktop/min_span_tree_visualizer/build/tests_include-b12d07c.cmake")
-subdirs("src")
-subdirs("lib/Catch2")
